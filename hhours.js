c594d8fb2bace595d975cf99407bb287558a07b2
Nathan Culwell-Kanarek <Nathan.Culwell-Kanarek@wicourts.gov>
2026-09-04 12:19:52 -0500

    Add lunch time

--- hhours.js
+++ hhours.js
@@ -2,11 +2,18 @@
 
 "strict";
 
-let totalWeekHours = 0;
-
 // The starting point for execution.
 function hoursInit() {
   addListeners();
+}
+
+function addListeners() {
+  const inputs = document.getElementsByTagName('input');
+  console.log(inputs);
+  for (input of inputs) {
+    input.value = '';
+    input.addEventListener('input', changeListener);
+  }
 }
 
 function getTimeInputs() {
@@ -14,15 +21,14 @@ function getTimeInputs() {
   const et = document.getElementById('end-times');
   const sti = st.querySelectorAll('input');
   const eti = et.querySelectorAll('input');
-  return [ sti, eti ];
+  const lunch = document.getElementById('lunch-hours');
+  return [ sti, eti, lunch ];
 }
 
 function changeListener(event) {
   console.log("CHANGED", event);
   const v = event.target.value;
-  console.log(v);
-  const n = parseInt(v);
-  console.log(n);
+  console.log(`Field changed to value: ${v}`);
   calculate();
 }
 
@@ -30,39 +36,27 @@ function sum(numbers) {
   return numbers.reduce((accum, current) => (accum + current), 0);
 }
 
-function calculate() {
-  const [sti, eti] = getTimeInputs();
-  console.log(sti);
-  const startTimes = Array.from(sti).map(input => parseInt(input.value));
-  const endTimes = Array.from(eti).map(input => parseInt(input.value));
-  const pairs = Iterator.zip([ startTimes, endTimes ]);
-  console.log(pairs);
-  const dayTimes = pairs.map(([s,e]) => (e - s)).filter(t => !Number.isNaN(t) && t>0);
-  const total = sum(dayTimes);
-  totalWeekHours = total;
-  console.log(`Total hours: ${total}`);
-  document.getElementById('total-hours').textContent = total.toString();
+function parse(v) {
+  return parseFloat(v);
 }
 
-function addListeners() {
-  const [s, e] = getTimeInputs();
-  console.log(s);
-  console.log(e);
-  let i = 0;
-  for (input of s) {
-    i++;
-    input.id = `start-time-day-${i}`;
-    input.dataset.dayNumber = i;
-    input.value = '';
-    input.addEventListener('input', changeListener);
-  }
-  i = 0;
-  for (input of e) {
-    i++;
-    input.id = `end-time-day-${i}`;
-    input.dataset.dayNumber = i;
-    input.value = '';
-    input.addEventListener('input', changeListener);
-  }
+function calculate() {
+  const [sti, eti, lunch] = getTimeInputs();
+  console.log(sti);
+  const startTimes = Array.from(sti).map(input => parse(input.value));
+  const endTimes = Array.from(eti).map(input => parse(input.value));
+  const pairs = Iterator.zip([ startTimes, endTimes ]);
+  console.log(pairs);
+  const dayTimes = pairs
+    .map(([s,e]) => (e - s))
+    .filter(t => !Number.isNaN(t) && t>0);
+  const lunchHours = parse(lunch.value);
+  console.log("Lunch duration:", lunchHours);
+  const total = (Number.isNaN(lunchHours)
+    ? sum(dayTimes)
+    : sum(dayTimes.map(t => t - lunchHours))
+  );
+  console.log(`Total hours: ${total}`);
+  document.getElementById('total-hours').textContent = total.toString();
 }
 
