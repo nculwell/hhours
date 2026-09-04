52118f9df12b8a16765ef68ed379424dce6cc5e5
Nathan Culwell-Kanarek <Nathan.Culwell-Kanarek@wicourts.gov>
2026-09-04 14:17:01 -0500

    Render schedule

--- hhours.js
+++ hhours.js
@@ -2,6 +2,8 @@
 
 "strict";
 
+const dayAbbrs = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+
 // The starting point for execution.
 function hoursInit() {
   addListeners();
@@ -43,20 +45,24 @@ function parse(v) {
 function calculate() {
   const [sti, eti, lunch] = getTimeInputs();
   console.log(sti);
-  const startTimes = Array.from(sti).map(input => parse(input.value));
-  const endTimes = Array.from(eti).map(input => parse(input.value));
-  const pairs = Iterator.zip([ startTimes, endTimes ]);
-  console.log(pairs);
-  const dayTimes = pairs
-    .map(([s,e]) => (e - s))
-    .filter(t => !Number.isNaN(t) && t>0);
-  const lunchHours = parse(lunch.value);
-  console.log("Lunch duration:", lunchHours);
-  const total = (Number.isNaN(lunchHours)
-    ? sum(dayTimes)
-    : sum(dayTimes.map(t => t - lunchHours))
+  const [startTimes, endTimes] = (
+    [sti, eti].map(timeInputs =>
+      Array.from(timeInputs).map(input => parse(input.value)))
   );
+  let days = Iterator.zip([ dayAbbrs, startTimes, endTimes ]);
+  days = Array.from(days.map(([d,s,e]) => ({ day: d, st: s, et: e, diff: e-s })));
+  console.log('days:', days);
+  const daysWithHours = days.filter(d => !Number.isNaN(d.diff) && d.diff > 0);
+  console.log('daysWithHours:', daysWithHours);
+  let lunchHours = parse(lunch.value);
+  if (Number.isNaN(lunchHours) || lunchHours <= 0) {
+    lunchHours = 0;
+  }
+  console.log("Lunch duration:", lunchHours);
+  const total = sum(daysWithHours.map(d => d.diff - lunchHours));
   console.log(`Total hours: ${total}`);
   document.getElementById('total-hours').textContent = total.toString();
+  let sched = days.map(d => `${d.day}: ${d.st} - ${d.et} (${d.diff})`).join("<BR>");
+  document.getElementById('schedule').setHTML(sched);
 }
 
