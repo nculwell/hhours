5365b83b6b886f994f70567a99f0a79c3a3b9807
Nathan Culwell-Kanarek <Nathan.Culwell-Kanarek@wicourts.gov>
2026-09-04 12:06:44 -0500

    Basic calculation works

--- hhours.js
+++ hhours.js
@@ -9,10 +9,12 @@ function hoursInit() {
   addListeners();
 }
 
-function getInputs() {
-  const tr = document.getElementById('hours-row');
-  const inputs = tr.querySelectorAll('input');
-  return inputs;
+function getTimeInputs() {
+  const st = document.getElementById('start-times');
+  const et = document.getElementById('end-times');
+  const sti = st.querySelectorAll('input');
+  const eti = et.querySelectorAll('input');
+  return [ sti, eti ];
 }
 
 function changeListener(event) {
@@ -24,28 +26,40 @@ function changeListener(event) {
   calculate();
 }
 
+function sum(numbers) {
+  return numbers.reduce((accum, current) => (accum + current), 0);
+}
+
 function calculate() {
-  const inputs = getInputs();
-  let total = 0;
-  for (input of inputs) {
-    const dayHours = parseInt(input.value);
-    if (!Number.isNaN(dayHours)) {
-      console.log(`dayHours (${input.dataset.dayNumber}): ${dayHours}`);
-      total += dayHours;
-    }
-  }
+  const [sti, eti] = getTimeInputs();
+  console.log(sti);
+  const startTimes = Array.from(sti).map(input => parseInt(input.value));
+  const endTimes = Array.from(eti).map(input => parseInt(input.value));
+  const pairs = Iterator.zip([ startTimes, endTimes ]);
+  console.log(pairs);
+  const dayTimes = pairs.map(([s,e]) => (e - s)).filter(t => !Number.isNaN(t) && t>0);
+  const total = sum(dayTimes);
   totalWeekHours = total;
   console.log(`Total hours: ${total}`);
   document.getElementById('total-hours').textContent = total.toString();
 }
 
 function addListeners() {
-  const inputs = getInputs();
-  console.log(inputs);
+  const [s, e] = getTimeInputs();
+  console.log(s);
+  console.log(e);
   let i = 0;
-  for (input of inputs) {
+  for (input of s) {
     i++;
-    input.id = `input-day-${i}`;
+    input.id = `start-time-day-${i}`;
+    input.dataset.dayNumber = i;
+    input.value = '';
+    input.addEventListener('input', changeListener);
+  }
+  i = 0;
+  for (input of e) {
+    i++;
+    input.id = `end-time-day-${i}`;
     input.dataset.dayNumber = i;
     input.value = '';
     input.addEventListener('input', changeListener);
